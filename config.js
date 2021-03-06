5946aaf8c9b9aef791dfa267a740cc5788937340
Shadow <william.shadow@outlook.com>
2021-03-06 11:40:54 +0000

    stuff happened

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    
+    ghost: 'https://admin.talktothetree.xyz'
 }
--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require("express")
 const app = new express()
 const bodyParser = require("body-parser")
 const config = require("./config.js")
+const GhostContentAPI = require('@tryghost/content-api');
+const api = new GhostContentAPI({
+  url: config.ghost,
+  key: process.env.GHOST,
+  version: "v3"
+});
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -10,8 +16,20 @@ app.use(express.static("public"))
 
 app.set("view engine", "ejs")
 
-app.get("/", (req, res) => {
-  res.render("index", {config})
-})
+const run = async () => {
+  
 
-app.listen(process.env.port, () => console.log("Site online on port " + process.env.port))
+  app.get("/", async (req, res) => {
+    let announcement = await api.posts.browse({limit: 1, filter: 'featured:true'});
+    let podcasts = await api.posts.browse({limit: 50, filter: 'tag:podcast'});
+    console.log(podcasts)
+
+    res.render("index", { config, announcement, podcasts })
+  })
+
+
+  app.listen(process.env.PORT, () => console.log("Site online on port " + process.env.PORT))
+
+}
+
+run()
